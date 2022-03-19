8d1408417f24ca11aad6acffd37d6899a4a6497a
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-19 18:02:19 -0400

    refactor: move creation sut to a factory method helper

--- tests/utils/validation/email-validation.spec.ts
+++ tests/utils/validation/email-validation.spec.ts
@@ -32,10 +32,23 @@ class EmailValidation {
   }
 }
 
+type SutTypes = {
+  sut: EmailValidation,
+  emailValidatorSpy: EmailValidatorSpy
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorSpy = new EmailValidatorSpy()
+  const sut = new EmailValidation(emailValidatorSpy)
+  return {
+    sut,
+    emailValidatorSpy
+  }
+}
+
 describe('Email Validation', () => {
   it('should return an erro if EmailValidator returns false', () => {
-    const emailValidatorSpy = new EmailValidatorSpy()
-    const sut = new EmailValidation(emailValidatorSpy)
+    const { sut, emailValidatorSpy } = makeSut()
     emailValidatorSpy.isEmailValid = false
     const email = faker.internet.email()
     const error = sut.validate(email)
