9892450416c5e903e9dc9ccc1e61706780b21232
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-19 01:01:34 -0400

    test: ensure SignUpController return 200 if valid data is provided

--- tests/controllers/signup-controller.spec.ts
+++ tests/controllers/signup-controller.spec.ts
@@ -1,15 +1,53 @@
 class SignUpController {
+  constructor(
+    private readonly validation: Validation
+  ) {}
+
   async handle(): Promise<any> {
-    return {
-      statusCode: 400
+    const isValid = this.validation.validate()
+    if (!isValid) {
+      return {
+        statusCode: 400
+      }
     }
+    return {
+      statusCode: 200
+    }
+  }
+}
+
+interface Validation {
+  validate: () => boolean
+}
+
+class ValidationSpy implements Validation {
+  result = true
+
+  validate(): boolean {
+    return this.result
+  }
+}
+
+const makeSut = () => {
+  const validationSpy = new ValidationSpy()
+  const sut = new SignUpController(validationSpy)
+  return {
+    sut,
+    validationSpy
   }
 }
 
 describe('SignUp Controller', () => {
   it('should return 400 if Validation returns false', async () => {
-    const sut = new SignUpController()
+    const { sut, validationSpy } = makeSut()
+    validationSpy.result = false
     const httpResponse = await sut.handle()
     expect(httpResponse.statusCode).toBe(400)
+  })
+
+  it('should return 200 if valid data is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle()
+    expect(httpResponse.statusCode).toBe(200)
   })
 })
