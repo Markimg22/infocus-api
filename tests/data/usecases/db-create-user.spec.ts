86dfbb38add58380c4271aa120c2ebfb2d91b9d5
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-20 18:49:53 -0400

    test: ensure DbCreateUser calls CheckUserByEmailRepository with correct email

--- tests/data/usecases/db-create-user.spec.ts
+++ tests/data/usecases/db-create-user.spec.ts
@@ -1,11 +1,16 @@
+import { CreateUser } from '@/domain/usecases'
+import faker from '@faker-js/faker'
+
 interface CheckUserByEmailRepository {
-  check: () => Promise<boolean>
+  check: (email: string) => Promise<boolean>
 }
 
 class CheckUserByEmailRepositorySpy implements CheckUserByEmailRepository {
   exists = false
+  email = ''
 
-  async check(): Promise<boolean> {
+  async check(email: string): Promise<boolean> {
+    this.email = email
     return this.exists
   }
 }
@@ -15,8 +20,8 @@ class DbCreateUser {
     private readonly checkUserByEmailRepository: CheckUserByEmailRepository
   ) {}
 
-  async create(): Promise<boolean> {
-    const exists = await this.checkUserByEmailRepository.check()
+  async create(params: CreateUser.Params): Promise<CreateUser.Result> {
+    const exists = await this.checkUserByEmailRepository.check(params.email)
     if (exists) {
       return false
     }
@@ -24,7 +29,18 @@ class DbCreateUser {
   }
 }
 
-const makeSut = () => {
+const mockCreateUser = (): CreateUser.Params => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
+
+type SutTypes = {
+  sut: DbCreateUser,
+  checkUserByEmailRepositorySpy: CheckUserByEmailRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
   const checkUserByEmailRepositorySpy = new CheckUserByEmailRepositorySpy()
   const sut = new DbCreateUser(checkUserByEmailRepositorySpy)
   return {
@@ -37,13 +53,20 @@ describe('DbCreateUser UseCase', () => {
   it('should return false if CheckUserByEmailRepository returns true', async () => {
     const { sut, checkUserByEmailRepositorySpy } = makeSut()
     checkUserByEmailRepositorySpy.exists = true
-    const result = await sut.create()
+    const result = await sut.create(mockCreateUser())
     expect(result).toBe(false)
   })
 
   it('should return true if CheckUserByEmailRepository returns false', async () => {
     const { sut } = makeSut()
-    const result = await sut.create()
+    const result = await sut.create(mockCreateUser())
     expect(result).toBe(true)
+  })
+
+  it('should call CheckUserByEmailRepository with correct email', async () => {
+    const { sut, checkUserByEmailRepositorySpy } = makeSut()
+    const fakeUser = mockCreateUser()
+    await sut.create(fakeUser)
+    expect(checkUserByEmailRepositorySpy.email).toBe(fakeUser.email)
   })
 })
