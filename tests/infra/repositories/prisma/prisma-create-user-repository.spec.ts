4ee53e7c9b8716e61cf3e940ee9219c3e3f819ac
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-21 02:19:46 -0400

    chore: config environment test for prisma

--- tests/infra/repositories/prisma/prisma-create-user-repository.spec.ts
+++ tests/infra/repositories/prisma/prisma-create-user-repository.spec.ts
@@ -1,37 +1,31 @@
 import { PrismaCreateUserRepository } from '@/infra/repositories/prisma'
 import { mockCreateUserParams, throwError } from '@/tests/domain/mocks'
-import { MockContext, Context, createMockContext, mockCreateUserDatabaseParams } from '@/tests/infra/mocks'
-
-let mockCtx: MockContext
-let ctx: Context
+import { client } from '@/infra/helpers'
 
 const makeSut = (): PrismaCreateUserRepository => {
-  const sut = new PrismaCreateUserRepository(ctx.prisma)
+  const sut = new PrismaCreateUserRepository(client)
   return sut
 }
 
 describe('PrismaCreateUser Repository', () => {
-  beforeEach(() => {
-    mockCtx = createMockContext()
-    ctx = mockCtx as unknown as Context
+  beforeAll(async () => {
+    await client.$connect()
+  })
+
+  afterAll(async () => {
+    await client.users.deleteMany()
+    await client.$disconnect()
   })
 
   it('should return true if user created on success', async () => {
     const sut = makeSut()
-    mockCtx.prisma.users.create.mockResolvedValue(mockCreateUserDatabaseParams())
     const result = await sut.create(mockCreateUserParams())
     expect(result).toBe(true)
   })
 
-  it('should return false if user not created', async () => {
-    const sut = makeSut()
-    const result = await sut.create(mockCreateUserParams())
-    expect(result).toBe(false)
-  })
-
   it('should throws if client database throws', async () => {
     const sut = makeSut()
-    jest.spyOn(ctx.prisma.users, 'create').mockImplementationOnce(throwError)
+    jest.spyOn(client.users, 'create').mockImplementationOnce(throwError)
     const result = sut.create(mockCreateUserParams())
     await expect(result).rejects.toThrow()
   })
