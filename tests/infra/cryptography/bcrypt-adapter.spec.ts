db0ef63febb09edd8a4fbf98c6437abcdc2b0707
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-23 12:58:07 -0400

    test: ensure BcryptAdapter throws if compare throws

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -76,5 +76,12 @@ describe('Bcrypt Adapter', () => {
       const isValid = await sut.compare('any_value', 'any_hash')
       expect(isValid).toBe(false)
     })
+
+    it('should throw if compare throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(throwError)
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
