69eb11af366f4953cdd7ab3bfb3024453cabeae3
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-23 12:52:46 -0400

    test: ensure BcryptAdapter returns true when compare succeds

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -63,5 +63,11 @@ describe('Bcrypt Adapter', () => {
       await sut.compare('any_value', 'any_hash')
       expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
     })
+
+    it('should return true when compare succeds', async () => {
+      const sut = makeSut()
+      const isValid = await sut.compare('any_value', 'any_hash')
+      expect(isValid).toBe(true)
+    })
   })
 })
