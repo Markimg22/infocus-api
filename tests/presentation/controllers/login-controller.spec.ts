05e9acb697beee348e2f5bad1bc9ccaad8b3da32
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-23 15:18:03 -0400

    test: ensure LoginController return 400 if Validation returns an error

---
+++ tests/presentation/controllers/login-controller.spec.ts
@@ -0,0 +1,56 @@
+import faker from '@faker-js/faker'
+import { MissingParamError } from '@/presentation/errors'
+import { badRequest } from '@/presentation/helpers'
+import { ValidationSpy } from '@/tests/presentation/mocks'
+import { Validation, HttpResponse } from '@/presentation/protocols'
+import { AuthenticationUser } from '@/domain/usecases'
+
+class LoginController {
+  constructor(
+    private readonly validation: Validation
+  ) {}
+
+  async handle(request: LoginController.Request): Promise<HttpResponse> {
+    const error = this.validation.validate(request)
+    if (error) return badRequest(error)
+    return {
+      statusCode: 200,
+      body: {}
+    }
+  }
+}
+
+namespace LoginController {
+  export type Request = {
+    email: string,
+    password: string
+  }
+}
+
+const mockRequest = (): AuthenticationUser.Params => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
+
+type SutTypes = {
+  sut: LoginController,
+  validationSpy: ValidationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy()
+  const sut = new LoginController(validationSpy)
+  return {
+    sut,
+    validationSpy
+  }
+}
+
+describe('Login Controller', () => {
+  it('return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new MissingParamError(faker.random.word())
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
+  })
+})
