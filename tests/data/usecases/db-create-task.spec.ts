9ec28ed312e14609f54ed36c82a5245aa3acd455
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-24 22:37:31 -0400

    test: ensure DbCreateTask calls CreateTaskRepository with corrrect values

---
+++ tests/data/usecases/db-create-task.spec.ts
@@ -0,0 +1,60 @@
+import faker from '@faker-js/faker'
+
+class DbCreateTask {
+  constructor(
+    private readonly createTaskRespository: CreateTaskRespository
+  ) {}
+
+  async create(params: CreateTask.Params): Promise<void> {
+    await this.createTaskRespository.create(params)
+  }
+}
+
+interface CreateTaskRespository {
+  create: (params: CreateTask.Params) => Promise<void>
+}
+
+class CreateTaskRespositorySpy implements CreateTaskRespository {
+  params = {}
+
+  async create(params: CreateTask.Params): Promise<void> {
+    this.params = params
+  }
+}
+
+namespace CreateTask {
+  export type Params = {
+    title: string,
+    description: string,
+    isCompleted: boolean,
+  }
+}
+
+const mockCreateTaskParams = (): CreateTask.Params => ({
+  title: faker.random.word(),
+  description: faker.random.word(),
+  isCompleted: false
+})
+
+type SutTypes = {
+  sut: DbCreateTask,
+  createTaskRespositorySpy: CreateTaskRespositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const createTaskRespositorySpy = new CreateTaskRespositorySpy()
+  const sut = new DbCreateTask(createTaskRespositorySpy)
+  return {
+    sut,
+    createTaskRespositorySpy
+  }
+}
+
+describe('DbCreateTask UseCase', () => {
+  it('should call CreateTaskRespository with correct values', async () => {
+    const { sut, createTaskRespositorySpy } = makeSut()
+    const createTaskParams = mockCreateTaskParams()
+    await sut.create(createTaskParams)
+    expect(createTaskRespositorySpy.params).toEqual(createTaskParams)
+  })
+})
