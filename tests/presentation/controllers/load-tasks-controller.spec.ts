c4f174c925a25a34b150252679c259b8a5f3ca50
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-26 11:31:47 -0400

    test: ensure LoadTasksController returns 500 if LoadTasks throws

--- tests/presentation/controllers/load-tasks-controller.spec.ts
+++ tests/presentation/controllers/load-tasks-controller.spec.ts
@@ -1,14 +1,26 @@
 import { LoadTasks } from '@/domain/usecases'
+import { throwError } from '@/tests/domain/mocks'
 import { LoadTasksSpy } from '@/tests/presentation/mocks'
+import { serverError } from '@/presentation/helpers'
+
 import faker from '@faker-js/faker'
+import { HttpResponse } from '../protocols'
 
 class LoadTasksController {
   constructor(
     private readonly loadTasks: LoadTasks
   ) {}
 
-  async handle(request: LoadTasksController.Request): Promise<void> {
-    await this.loadTasks.loadByUserId(request.userId)
+  async handle(request: LoadTasksController.Request): Promise<HttpResponse> {
+    try {
+      await this.loadTasks.loadByUserId(request.userId)
+      return {
+        statusCode: 200,
+        body: {}
+      }
+    } catch (error) {
+      return serverError(error as Error)
+    }
   }
 }
 
@@ -42,5 +54,12 @@ describe('LoadTasks Controller', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(loadTasksSpy.userId).toBe(request.userId)
+  })
+
+  it('should return 500 if LoadTasks throws', async () => {
+    const { sut, loadTasksSpy } = makeSut()
+    jest.spyOn(loadTasksSpy, 'loadByUserId').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
