2e7a01e8486ee6b82430da7b0571759adca9aaf2
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-26 16:42:55 -0400

    test: ensure DbUpdateStatusTask calls UpdateStatusTaskRepository with correct values

---
+++ tests/data/usecases/db-update-status-task.spec.ts
@@ -0,0 +1,59 @@
+import { UpdateStatusTask } from '@/domain/usecases'
+
+import faker from '@faker-js/faker'
+
+class DbUpdateStatusTask {
+  constructor(
+    private readonly updateStatusTaskRepository: UpdateStatusTaskRepository
+  ) {}
+
+  async update(data: UpdateStatusTask.Params): Promise<void> {
+    await this.updateStatusTaskRepository.update(data)
+  }
+}
+
+interface UpdateStatusTaskRepository {
+  update: (data: UpdateStatusTaskRepository.Params) => Promise<void>
+}
+
+namespace UpdateStatusTaskRepository {
+  export type Params = UpdateStatusTask.Params
+  export type Result = UpdateStatusTask.Result
+}
+
+class UpdateStatusTaskRepositorySpy implements UpdateStatusTaskRepository {
+  data = {}
+
+  async update(data: UpdateStatusTaskRepository.Params): Promise<void> {
+    this.data = data
+  }
+}
+
+const mockUpdateStatusTaskParams = (): UpdateStatusTask.Params => ({
+  id: faker.datatype.uuid(),
+  userId: faker.datatype.uuid(),
+  status: true
+})
+
+type SutTypes = {
+  sut: DbUpdateStatusTask,
+  updateStatusTaskRepositorySpy: UpdateStatusTaskRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const updateStatusTaskRepositorySpy = new UpdateStatusTaskRepositorySpy()
+  const sut = new DbUpdateStatusTask(updateStatusTaskRepositorySpy)
+  return {
+    sut,
+    updateStatusTaskRepositorySpy
+  }
+}
+
+describe('DbUpdateStatusTask UseCase', () => {
+  it('should call UpdateStatusTaskRepository with correct values', async () => {
+    const { sut, updateStatusTaskRepositorySpy } = makeSut()
+    const updateStatusTaskParams = mockUpdateStatusTaskParams()
+    await sut.update(updateStatusTaskParams)
+    expect(updateStatusTaskRepositorySpy.data).toEqual(updateStatusTaskParams)
+  })
+})
