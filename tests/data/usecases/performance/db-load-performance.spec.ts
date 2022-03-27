28380fc7ffa46d4462e454535ba94c3948c73977
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-27 18:24:28 -0400

    test: ensure DbLoadPerformance call LoadPerformanceRepository with correct userId

---
+++ tests/data/usecases/performance/db-load-performance.spec.ts
@@ -0,0 +1,46 @@
+import faker from '@faker-js/faker'
+
+class DbLoadPerformance {
+  constructor(
+    private readonly loadPerformanceRepository: LoadPerformanceRepository
+  ) {}
+
+  async load(userId: string): Promise<void> {
+    await this.loadPerformanceRepository.load(userId)
+  }
+}
+
+interface LoadPerformanceRepository {
+  load: (userId: string) => Promise<void>
+}
+
+class LoadPerformanceRepositorySpy implements LoadPerformanceRepository {
+  userId = ''
+
+  async load(userId: string): Promise<void> {
+    this.userId = userId
+  }
+}
+
+type SutTypes = {
+  sut: DbLoadPerformance,
+  loadPerformanceRepositorySpy: LoadPerformanceRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadPerformanceRepositorySpy = new LoadPerformanceRepositorySpy()
+  const sut = new DbLoadPerformance(loadPerformanceRepositorySpy)
+  return {
+    sut,
+    loadPerformanceRepositorySpy
+  }
+}
+
+describe('DbLoadPerformance UseCase', () => {
+  it('should call LoadPerformanceRepository with correct userId', async () => {
+    const { sut, loadPerformanceRepositorySpy } = makeSut()
+    const userId = faker.datatype.uuid()
+    await sut.load(userId)
+    expect(loadPerformanceRepositorySpy.userId).toBe(userId)
+  })
+})
