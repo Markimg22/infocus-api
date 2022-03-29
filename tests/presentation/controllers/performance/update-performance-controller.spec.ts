3d10607a015db51867faa3c55f3fa569af21017a
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-29 10:04:23 -0400

    test: ensure UpdatePerformanceController returns 200 on success

--- tests/presentation/controllers/performance/update-performance-controller.spec.ts
+++ tests/presentation/controllers/performance/update-performance-controller.spec.ts
@@ -1,4 +1,5 @@
-import { serverError } from '@/presentation/helpers'
+import { LoadPerformance } from '@/domain/usecases'
+import { ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 import { throwError } from '@/tests/domain/mocks'
 
@@ -11,11 +12,8 @@ class UpdatePerformanceController implements Controller {
 
   async handle(request: UpdatePerformanceController.Request): Promise<HttpResponse> {
     try {
-      await this.updatePerformance.update(request)
-      return {
-        statusCode: 200,
-        body: {}
-      }
+      const performanceUpdated = await this.updatePerformance.update(request)
+      return ok(performanceUpdated)
     } catch (error) {
       return serverError(error as Error)
     }
@@ -27,7 +25,7 @@ namespace UpdatePerformanceController {
 }
 
 interface UpdatePerformance {
-  update: (params: UpdatePerformance.Params) => Promise<void>
+  update: (params: UpdatePerformance.Params) => Promise<UpdatePerformance.Result>
 }
 
 namespace UpdatePerformance {
@@ -36,13 +34,21 @@ namespace UpdatePerformance {
     field: 'totalWorkTime' | 'totalRestTime' | 'totalTasksFinished',
     value: number
   }
+
+  export type Result = LoadPerformance.Result
 }
 
 class UpdatePerformanceSpy implements UpdatePerformance {
   params = {}
+  result = {
+    totalRestTime: faker.datatype.number(),
+    totalWorkTime: faker.datatype.number(),
+    totalTasksFinished: faker.datatype.number()
+  } as UpdatePerformance.Result
 
-  async update(params: UpdatePerformance.Params): Promise<void> {
+  async update(params: UpdatePerformance.Params): Promise<UpdatePerformance.Result> {
     this.params = params
+    return this.result
   }
 }
 
@@ -79,5 +85,11 @@ describe('UpdatePerformance Controller', () => {
     jest.spyOn(updatePerformanceSpy, 'update').mockImplementationOnce(throwError)
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  it('should return 200 on success', async () => {
+    const { sut, updatePerformanceSpy } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(ok(updatePerformanceSpy.result))
   })
 })
