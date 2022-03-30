96d12b4bc5e7da3b2e5a304a38cb8b868717a767
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-30 00:45:22 -0400

    test: ensure DbLoadUserByToken calls LoadUserByTokenRepository with correct values

--- tests/data/usecases/user/db-load-user-by-token.spec.ts
+++ tests/data/usecases/user/db-load-user-by-token.spec.ts
@@ -4,12 +4,16 @@ import faker from '@faker-js/faker'
 
 class DbLoadUserByToken {
   constructor(
-    private readonly decrypter: Decrypter
+    private readonly decrypter: Decrypter,
+    private readonly loadUserByTokenRepository: LoadUserByTokenRepository
   ) {}
 
   async load(params: LoadUserByToken.Params): Promise<LoadUserByToken.Result | null> {
-    const { accessToken } = params
-    await this.decrypter.decrypt(accessToken)
+    const { accessToken, role } = params
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      await this.loadUserByTokenRepository.load({ accessToken, role })
+    }
     return null
   }
 }
@@ -28,17 +32,36 @@ class DecrypterSpy implements Decrypter {
   }
 }
 
+interface LoadUserByTokenRepository {
+  load: (data: LoadUserByTokenRepository.Params) => Promise<void>
+}
+
+namespace LoadUserByTokenRepository {
+  export type Params = LoadUserByToken.Params
+}
+
+class LoadUserByTokenRepositorySpy implements LoadUserByTokenRepository {
+  data = {}
+
+  async load(data: LoadUserByTokenRepository.Params): Promise<void> {
+    this.data = data
+  }
+}
+
 type SutTypes = {
   sut: DbLoadUserByToken,
-  decrypterSpy: DecrypterSpy
+  decrypterSpy: DecrypterSpy,
+  loadUserByTokenRepositorySpy: LoadUserByTokenRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const decrypterSpy = new DecrypterSpy()
-  const sut = new DbLoadUserByToken(decrypterSpy)
+  const loadUserByTokenRepositorySpy = new LoadUserByTokenRepositorySpy()
+  const sut = new DbLoadUserByToken(decrypterSpy, loadUserByTokenRepositorySpy)
   return {
     sut,
-    decrypterSpy
+    decrypterSpy,
+    loadUserByTokenRepositorySpy
   }
 }
 
@@ -62,5 +85,13 @@ describe('DbLoadUserByToken UseCase', () => {
     decrypterSpy.plainText = null
     const user = await sut.load({ accessToken: token, role })
     expect(user).toBeNull()
+  })
+
+  it('should call LoadUserByTokenRepository with correct values', async () => {
+    const { sut, loadUserByTokenRepositorySpy } = makeSut()
+    await sut.load({ accessToken: token, role })
+    expect(loadUserByTokenRepositorySpy.data).toEqual({
+      accessToken: token, role
+    })
   })
 })
