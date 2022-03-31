6a2d42f155039fb57883e56e56cf3da30311a3b7
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 14:57:29 -0400

    feat: exports factories

--- src/main/factories/usecases/index.ts
+++ src/main/factories/usecases/index.ts
@@ -1,2 +1,5 @@
 export * from './create-user-factory'
 export * from './authentication-user-factory'
+export * from './create-task-factory'
+export * from './load-tasks-factory'
+export * from './load-user-by-token-factory'
