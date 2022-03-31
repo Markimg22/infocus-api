98d7b4fe322edf4a5813a0f15a5a247c8f45ba58
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 11:31:15 -0400

    feat: create login controller factory

---
+++ src/main/factories/controllers/login-controller-factory.ts
@@ -0,0 +1,11 @@
+import { LoginController } from '@/presentation/controllers'
+import { Controller } from '@/presentation/protocols'
+import { makeDbAuthenticationUser, makeLoginValidation } from '@/main/factories'
+
+export const makeLoginController = (): Controller => {
+  const controller = new LoginController(
+    makeLoginValidation(),
+    makeDbAuthenticationUser()
+  )
+  return controller
+}
