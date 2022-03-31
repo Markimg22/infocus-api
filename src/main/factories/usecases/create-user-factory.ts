2554aaa26ce1dbd5dc9aba8ef97b865f16970692
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 10:52:39 -0400

    feat: create factory create user

---
+++ src/main/factories/usecases/create-user-factory.ts
@@ -0,0 +1,20 @@
+import { DbCreateUser } from '@/data/usecases'
+import { CreateUser } from '@/domain/usecases'
+import { BcryptAdapter } from '@/infra/cryptography'
+import { makeRepositories } from '@/main/factories'
+
+export const makeDbCreateUser = (): CreateUser => {
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const {
+    checkUserByEmailRepository,
+    createUserRepository,
+    createPerformanceRepository
+  } = makeRepositories()
+  return new DbCreateUser(
+    checkUserByEmailRepository,
+    bcryptAdapter,
+    createUserRepository,
+    createPerformanceRepository
+  )
+}
---
+++ src/main/factories/usecases/index.ts
@@ -0,0 +1,2 @@
+export * from './create-user-factory'
+export * from './authentication-user-factory'
