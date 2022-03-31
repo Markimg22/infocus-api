9cc22a71e06d84f550a844a05581ed45d1d6c809
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 14:56:29 -0400

    feat: create factory create-task

---
+++ src/main/factories/usecases/create-task-factory.ts
@@ -0,0 +1,8 @@
+import { DbCreateTask } from '@/data/usecases'
+import { CreateTask } from '@/domain/usecases'
+import { makeRepositories } from '@/main/factories'
+
+export const makeDbCreateTask = (): CreateTask => {
+  const { createTaskRespository } = makeRepositories()
+  return new DbCreateTask(createTaskRespository)
+}
