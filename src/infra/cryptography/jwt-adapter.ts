7ffaaa32890894054d47620b763b03b0eeb9b9f1
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 14:52:53 -0400

    feat: add decrypter in jwt-adapter

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -1,11 +1,15 @@
-import { Encrypter } from '@/data/protocols/cryptography'
+import { Decrypter, Encrypter } from '@/data/protocols/cryptography'
 
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor(
     private readonly secret: string
   ) {}
+
+  async decrypt(cipherText: string): Promise<string | null> {
+    return jwt.verify(cipherText, this.secret) as any
+  }
 
   async encrypt(plainText: string): Promise<string> {
     return jwt.sign({ id: plainText }, this.secret)
