34943ac35bf162f3da97c71312f855eb7e6b801a
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 16:08:55 -0400

    test: ensure route /create-task returns 200 on create task

---
+++ tests/main/routes/tasks-routes.test.ts
@@ -0,0 +1,52 @@
+import { client } from '@/infra/helpers'
+import { setupApp } from '@/main/config/app'
+import { mockCreateUserParams } from '@/tests/domain/mocks'
+import { env } from '@/main/config/env'
+
+import { Express } from 'express'
+import { sign } from 'jsonwebtoken'
+import request from 'supertest'
+
+let app: Express
+
+const mockAccessToken = async (): Promise<string> => {
+  const user = await client.users.create({
+    data: mockCreateUserParams()
+  })
+  const accessToken = sign(user.id, env.jwtSecret)
+  await client.accessToken.create({
+    data: {
+      userId: user.id,
+      token: accessToken
+    }
+  })
+  return accessToken
+}
+
+describe('Tasks Routes', () => {
+  beforeAll(async () => {
+    app = await setupApp()
+    await client.$connect()
+  })
+
+  afterAll(async () => {
+    await client.accessToken.deleteMany()
+    await client.tasks.deleteMany()
+    await client.users.deleteMany()
+    await client.$disconnect()
+  })
+
+  describe('POST /create-task', () => {
+    it('should return 200 on create task', async () => {
+      const accessToken = await mockAccessToken()
+      await request(app)
+        .post('/api/create-task')
+        .set('x-access-token', accessToken)
+        .send({
+          title: 'Title Test',
+          description: 'Description Test'
+        })
+        .expect(200)
+    })
+  })
+})
