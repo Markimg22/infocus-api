5554735e297d201f74a312e1f29c4fc2fbb7a4c2
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 11:31:47 -0400

    feat: create login router

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,11 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignupController } from '@/main/factories'
+import { makeSignupController, makeLoginController } from '@/main/factories'
 
 import { Router, Express } from 'express'
 
 export const setupRoutes = (app: Express): void => {
   const router = Router()
   router.post('/signup', adaptRoute(makeSignupController()))
+  router.post('/login', adaptRoute(makeLoginController()))
   app.use('/api', router)
 }
