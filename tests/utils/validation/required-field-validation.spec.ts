c3261785a0f230d50059d9b0b98f22075e8b9de1
Markimg22 <marquim.net.221200@hotmail.com>
2022-03-31 10:50:01 -0400

    feat: create validations compare fields, required fields

---
+++ tests/utils/validation/required-field-validation.spec.ts
@@ -0,0 +1,24 @@
+import { RequiredFieldValidation } from '@/utils/validation'
+import { MissingParamError } from '@/presentation/errors'
+
+import faker from '@faker-js/faker'
+
+const field = faker.random.word()
+
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation(field)
+}
+
+describe('RequiredField Validation', () => {
+  it('should return a MissingParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({ invalidField: faker.random.word() })
+    expect(error).toEqual(new MissingParamError(field))
+  })
+
+  it('should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ [field]: faker.random.word() })
+    expect(error).toBeFalsy()
+  })
+})
