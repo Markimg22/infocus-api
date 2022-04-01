fdce99ead49b7e0cad880ecc6b4e748c14d3fba9
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-01 11:18:59 -0400

    feat: implements /update-status-task in tasks routes

--- src/main/routes/tasks-routes.ts
+++ src/main/routes/tasks-routes.ts
@@ -1,7 +1,8 @@
 import { adaptRoute } from '@/main/adapters'
 import {
   makeCreateTaskController,
-  makeLoadTasksController
+  makeLoadTasksController,
+  makeUpdateStatusTaskController
 } from '@/main/factories'
 import { auth } from '@/main/middlewares'
 
@@ -10,4 +11,5 @@ import { Router } from 'express'
 export default (router: Router): void => {
   router.post('/create-task', auth, adaptRoute(makeCreateTaskController()))
   router.post('/load-tasks', auth, adaptRoute(makeLoadTasksController()))
+  router.put('/update-status-task', auth, adaptRoute(makeUpdateStatusTaskController()))
 }
