9da3809b3f172bb59f2cc99272cdbcb73f184663
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-01 11:18:25 -0400

    feat: create factory update status task validation

---
+++ src/main/factories/validations/update-status-task-validation-factory.ts
@@ -0,0 +1,10 @@
+import { Validation } from '@/presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '@/utils/validation'
+
+export const makeUpdateStatusTaskValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['id']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
