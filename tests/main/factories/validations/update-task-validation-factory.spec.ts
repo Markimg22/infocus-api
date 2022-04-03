42ea8b8fabf174e90f91cc9c0ff4f46670f6e4d5
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-03 10:58:59 -0400

    test: ensure UpdateStatusTaskFactory calls ValidationComposite with all validations

---
+++ tests/main/factories/validations/update-task-validation-factory.spec.ts
@@ -0,0 +1,16 @@
+import { makeUpdateStatusTaskValidation } from '@/main/factories'
+import { Validation } from '@/presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '@/utils/validation'
+
+jest.mock('@/utils/validation/validation-composite')
+
+describe('UpdateStatusTaskValidation Factory', () => {
+  it('should call ValidationComposite with all validations', () => {
+    makeUpdateStatusTaskValidation()
+    const validations: Validation[] = []
+    for (const field of ['id', 'finished']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
