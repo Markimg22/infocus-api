c903afde52ed870b424ea0774e11e3fe55ce983a
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-04 15:35:29 -0400

    feat: create factory update performance controller

---
+++ src/main/factories/controllers/update-performance-controller-factory.ts
@@ -0,0 +1,8 @@
+import { UpdatePerformanceController } from '@/presentation/controllers'
+import { Controller } from '@/presentation/protocols'
+import { makeDbLoadPerformance, makeDbUpdatePerformance } from '@/main/factories'
+
+export const makeUpdatePerformanceController = (): Controller => {
+  const controller = new UpdatePerformanceController(makeDbUpdatePerformance(), makeDbLoadPerformance())
+  return controller
+}
