cf7711ae7ac09750d5c57feef412ee6c0e0e9ac5
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-04 12:14:06 -0400

    feat: create load performance factory controller

---
+++ src/main/factories/controllers/load-performance-controller-factory.ts
@@ -0,0 +1,8 @@
+import { LoadPerformanceController } from '@/presentation/controllers'
+import { Controller } from '@/presentation/protocols'
+import { makeDbLoadPerformance } from '@/main/factories'
+
+export const makeLoadPerformanceController = (): Controller => {
+  const controller = new LoadPerformanceController(makeDbLoadPerformance())
+  return controller
+}
