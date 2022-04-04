48cb528eb2bf897b21d790cc6a381e182a0a10ba
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-04 15:36:18 -0400

    feat: create route /update-performance

--- src/main/routes/performance-routes.ts
+++ src/main/routes/performance-routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeLoadPerformanceController } from '@/main/factories'
+import { makeLoadPerformanceController, makeUpdatePerformanceController } from '@/main/factories'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.get('/load-performance', auth, adaptRoute(makeLoadPerformanceController()))
+  router.put('/update-performance', auth, adaptRoute(makeUpdatePerformanceController()))
 }
