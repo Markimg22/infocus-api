9deca23819fbbcbbfcd62f7257ef68bf066e6080
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-04 23:27:04 -0400

    fix: correction config logs

--- src/utils/log/logger.ts
+++ src/utils/log/logger.ts
@@ -1,7 +1,8 @@
 import pino from 'pino'
 
 export const logger = pino({
-  enabled: true,
+  // eslint-disable-next-line no-extra-boolean-cast
+  enabled: !(!!process.env.LOG_DISABLED),
   transport: {
     target: 'pino-pretty',
     options: {
