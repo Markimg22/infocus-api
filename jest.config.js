1ba5b7477cf90d923c2e8d1d04274a2ed6cbdfd7
Markimg22 <marquim.net.221200@hotmail.com>
2022-04-29 19:56:42 -0400

    style: implements new config eslint and prettier

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest'
+    '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
-  }
-}
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
