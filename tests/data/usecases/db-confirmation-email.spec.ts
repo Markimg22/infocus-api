e0819cb627c96f0fbe549939b555e45dfe83aae0
Markimg22 <marquim.net.221200@hotmail.com>
2022-05-03 22:23:14 -0400

    test: ensure DbConfirmationEmail calls UpdateUserEmailConfirmatedRepository with correct values

--- tests/data/usecases/db-confirmation-email.spec.ts
+++ tests/data/usecases/db-confirmation-email.spec.ts
@@ -3,40 +3,90 @@ import faker from '@faker-js/faker';
 
 class DbConfirmationEmail {
   constructor(
-    private readonly loadUserByConfirmationCodeRepository: LoadUserByConfirmationCodeRepository
+    private readonly loadUserByConfirmationCodeRepository: LoadUserByConfirmationCodeRepository,
+    private readonly updateUserEmailConfirmatedRepository: UpdateUserEmailConfirmatedRepository
   ) {}
 
   async confirm(code: string): Promise<void> {
-    await this.loadUserByConfirmationCodeRepository.load(code);
+    const user = await this.loadUserByConfirmationCodeRepository.load(code);
+    await this.updateUserEmailConfirmatedRepository.update({
+      id: user.id,
+      emailConfirmated: true,
+    });
+  }
+}
+
+export interface UpdateUserEmailConfirmatedRepository {
+  update: (data: UpdateUserEmailConfirmatedRepository.Params) => Promise<void>;
+}
+
+export namespace UpdateUserEmailConfirmatedRepository {
+  export type Params = {
+    id: string;
+    emailConfirmated: boolean;
+  };
+}
+
+class UpdateUserEmailConfirmatedRepositorySpy
+  implements UpdateUserEmailConfirmatedRepository
+{
+  data: UpdateUserEmailConfirmatedRepository.Params = {
+    id: faker.datatype.uuid(),
+    emailConfirmated: true,
+  };
+
+  async update(
+    data: UpdateUserEmailConfirmatedRepository.Params
+  ): Promise<void> {
+    this.data = data;
   }
 }
 
 export interface LoadUserByConfirmationCodeRepository {
-  load: (code: string) => Promise<void>;
+  load: (code: string) => Promise<LoadUserByConfirmationCodeRepository.Result>;
+}
+
+export namespace LoadUserByConfirmationCodeRepository {
+  export type Result = {
+    id: string;
+  };
 }
 
 class LoadUserByConfirmationCodeRepositorySpy
   implements LoadUserByConfirmationCodeRepository
 {
   confirmationCode = '';
+  result = {
+    id: faker.datatype.uuid(),
+  };
 
-  async load(code: string): Promise<void> {
+  async load(
+    code: string
+  ): Promise<LoadUserByConfirmationCodeRepository.Result> {
     this.confirmationCode = code;
+    return this.result;
   }
 }
 
 type SutTypes = {
   sut: DbConfirmationEmail;
   loadUserByConfirmationCodeRepositorySpy: LoadUserByConfirmationCodeRepositorySpy;
+  updateUserEmailConfirmatedRepositorySpy: UpdateUserEmailConfirmatedRepositorySpy;
 };
 
 const makeSut = (): SutTypes => {
   const loadUserByConfirmationCodeRepositorySpy =
     new LoadUserByConfirmationCodeRepositorySpy();
-  const sut = new DbConfirmationEmail(loadUserByConfirmationCodeRepositorySpy);
+  const updateUserEmailConfirmatedRepositorySpy =
+    new UpdateUserEmailConfirmatedRepositorySpy();
+  const sut = new DbConfirmationEmail(
+    loadUserByConfirmationCodeRepositorySpy,
+    updateUserEmailConfirmatedRepositorySpy
+  );
   return {
     sut,
     loadUserByConfirmationCodeRepositorySpy,
+    updateUserEmailConfirmatedRepositorySpy,
   };
 };
 
@@ -57,5 +107,18 @@ describe('DbConfirmationEmail', () => {
       .mockImplementationOnce(throwError);
     const promise = sut.confirm(faker.datatype.uuid());
     await expect(promise).rejects.toThrow();
+  });
+
+  it('should call UpdateUserEmailConfirmatedRepository with correct values', async () => {
+    const {
+      sut,
+      loadUserByConfirmationCodeRepositorySpy,
+      updateUserEmailConfirmatedRepositorySpy,
+    } = makeSut();
+    await sut.confirm(faker.datatype.uuid());
+    expect(updateUserEmailConfirmatedRepositorySpy.data).toEqual({
+      id: loadUserByConfirmationCodeRepositorySpy.result.id,
+      emailConfirmated: true,
+    });
   });
 });
