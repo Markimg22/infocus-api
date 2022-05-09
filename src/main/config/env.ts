1da359797d3362dd99910bd47173247f1e0173b9
Markimg22 <marquim.net.221200@hotmail.com>
2022-05-09 12:28:54 -0400

    chore: add dotenv and config mail envs

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const env = {
   apiUrl: process.env.API_URL || 'http://localhost:8080',
   databaseUrl:
@@ -5,9 +9,9 @@ export const env = {
     'postgresql://admin:admin@localhost:5432/database',
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || 'ASDg1@eff=13-5FASDA',
-  mailFrom: process.env.MAIL_FROM || 'smtp.mailtrap.io',
-  mailHost: process.env.MAIL_HOST || 'smtp.mailtrap.io',
-  mailPort: process.env.MAIL_PORT || 2525,
-  mailPassword: process.env.MAIL_PASSWORD || '660dedd174c71f',
-  mailUsername: process.env.MAIL_USERNAME || '0582842759e264',
+  mailFrom: process.env.MAIL_FROM || 'from@example.com',
+  mailHost: process.env.MAIL_HOST || '',
+  mailPort: Number(process.env.MAIL_PORT) || 2525,
+  mailUsername: process.env.MAIL_USERNAME || '',
+  mailPassword: process.env.MAIL_PASSWORD || '',
 };
