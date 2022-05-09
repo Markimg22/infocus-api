4bbd07f0d45d85d90a860ca070cb209b129679d5
Markimg22 <marquim.net.221200@hotmail.com>
2022-05-08 20:33:50 -0400

    chore: add email envs in config env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,13 @@
 export const env = {
+  apiUrl: process.env.API_URL || 'http://localhost:8080',
   databaseUrl:
     process.env.DATABASE_URL ||
     'postgresql://admin:admin@localhost:5432/database',
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || 'ASDg1@eff=13-5FASDA',
+  mailFrom: process.env.MAIL_FROM || '',
+  mailHost: process.env.MAIL_HOST || '',
+  mailPort: process.env.MAIL_PORT || 1000,
+  mailPassword: process.env.MAIL_PASSWORD || '',
+  mailUsername: process.env.MAIL_USERNAME || '',
 };
