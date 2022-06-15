268a9b87df9f4464af3c34ec04be185494b27190
Markimg22 <marquim.net.221200@hotmail.com>
2022-06-14 20:20:10 -0400

    feat: create enum HttpStatusCode

--- src/presentation/protocols/http-response.ts
+++ src/presentation/protocols/http-response.ts
@@ -1,4 +1,13 @@
-export type HttpResponse = {
-  statusCode: number;
-  body: any;
+export type HttpResponse<B = any> = {
+  statusCode: HttpStatusCode;
+  body: B;
 };
+
+export enum HttpStatusCode {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  SERVER_ERROR = 500,
+}
