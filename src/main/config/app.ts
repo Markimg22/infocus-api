9a343165b36e549b5e6622ce1dc21cdf87d2e2b1
Markimg22 <marquim.net.221200@hotmail.com>
2022-07-11 00:00:13 -0400

    chore: config swagger

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,14 @@
 import { setupMiddlewares } from '@/main/config/middlewares';
 import { setupRoutes } from '@/main/config/routes';
+import { setupSwagger } from '@/main/config/swagger';
+import { setupStaticFiles } from '@/main/config/static-files';
 
 import express, { Express } from 'express';
 
 export const setupApp = async (): Promise<Express> => {
   const app = express();
+  setupStaticFiles(app);
+  setupSwagger(app);
   setupMiddlewares(app);
   setupRoutes(app);
   return app;
